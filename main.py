c232bac78cac909ae396bed18385db7d9a501981
Glassy <61170320+isoooopo@users.noreply.github.com>
2023-03-22 19:35:29 -0400

    Update main.py

--- main.py
+++ main.py
@@ -6,8 +6,8 @@ if __name__ == '__main__':
     flag = True
     while flag == True:
 
-        #Encoder
 
+        #Menu
         print("Menu")
         print("-------------")
         print("1. Encode")
@@ -18,6 +18,9 @@ if __name__ == '__main__':
         option = int(input("Please enter an option: "))
 
         if option == 1:
+
+            #Encoder
+
             password = input("Please enter your password to decode: ")
             for i in password:
                 if int(i) < 7:
@@ -25,11 +28,16 @@ if __name__ == '__main__':
                 elif int(i) >= 7:
                     encoded_password += str((int(i) +3) % 10)
 
+            # Changing it so can't just call OG password for decode
+            password = encoded_password
+
             print("Your password has been encoded and stored!")
             print("")
         elif option == 2:
-            print(f"The encoded password is {encoded_password}, and the original password is {password}.")
-            print("")
+            #Decoder
+            
+
+
         elif option == 3:
             break
 
